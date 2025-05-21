6d19991a577089c92d12e6984f8f393311a33339
AnkanCompiled <ankanb560@gmail.com>
2025-05-21 18:26:45 +0530

    Enhance page layout and styling; update image source and improve banner content

--- app/page.js
+++ app/page.js
@@ -19,18 +19,33 @@ export default function page() {
           </Link>
         ))}
       </div>
-      <div className="relative h-[270px] sm:h-[360px] lg:h-[540px]">
+      <div className="relative h-[270px] sm:h-[360px] lg:h-[540px] xl:h-[720px] bg-[#111]">
         <Image
-          src="https://images.pexels.com/photos/9836576/pexels-photo-9836576.jpeg?auto=compress&cs=tinysrgb&w=3500&h=2333&dpr=1"
+          src="https://images.pexels.com/photos/9836576/pexels-photo-9836576.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
           fill
           priority
-          className="object-cover object-center"
+          className="object-cover object-center shadow-lg opacity-50"
           alt="banner"
         />
-        <div className="absolute top-1/4 left-1/2 -translate-x-1/2">
-          <span className="font-bold text-white text-4xl sm:text-5xl lg:text-6xl drop-shadow-lg">
-            Temp Text
-          </span>
+        <div className="absolute h-full w-full grid grid-rows-2">
+          <div className="flex flex-col items-center justify-center text-center m-4 space-y-4 h-full">
+            <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl font-bold">
+              Fashion that Speaks You
+            </h1>
+            <p className="text-white text-sm sm:text-base lg:text-lg font-mono mt-2">
+              Discover the latest trends in fashion
+            </p>
+          </div>
+          <div className="flex items-center lg:items-baseline justify-center h-full">
+            <Link
+              href="/new"
+              className="bg-white px-8 py-2 rounded-md shadow-md hover:bg-gray-200 duration-200"
+            >
+              <span className="text-xl text-neutral-700 font-semibold uppercase">
+                shop the collection
+              </span>
+            </Link>
+          </div>
         </div>
       </div>
     </>
